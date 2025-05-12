007d4c167c4447b7ed5255f95580c329dbb6d377
Perttu Lähteenlahti <perttu@lahteenlahti.com>
2025-05-12 19:39:30 +0200

    chore: different command

--- idx-template.nix
+++ idx-template.nix
@@ -11,15 +11,14 @@
   bootstrap = ''
     mkdir "$out"
     mkdir -p "$out/.idx/"
-    cp -rf ${./.idx/dev.nix} "$out/.idx/dev.nix"
-    shopt -s dotglob; cp -r ${./.idx}/* "$out"
+    cp -rf ${./template/dev.nix} "$out/.idx/dev.nix"
 
     cd "$out"
-    npx create-expo-app@"latest" "${projectName}" --template blank@sdk-52
-    cd "${projectName}"
+    npx create-expo-app@"latest" . --template blank@sdk-52
     yarn add react-native-purchases
     npx expo install expo-dev-client
-    npx expo run:android
+    npx expo prebuild --platform android
+    cd ios && pod install || true
     chmod -R +w "$out"
   '';
 }
